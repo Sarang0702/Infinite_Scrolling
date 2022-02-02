d1c131d95b0ede2cf6fd7ea724f8222f908eaff8
Sarang Gunjal <72076630+Sarang0702@users.noreply.github.com>
2022-02-02 11:29:52 +0530

    ...

--- index.js
+++ index.js
@@ -11,19 +11,55 @@ const getPost = async () =>
    
    data.map((curElm, index) =>
    {
-       const htmlData = `
-       <div class="posts">
-            <p class="post-id">${postCount++}</p>
-            <h2 class="title">${curElm.title}</h2>
-            <p class="post-info">${curElm.body}</p>
-       </div>
-       `;
+    //    const htmlData = `
+    //    <div class="posts">
+    //         <p class="post-id">${postCount++}</p>
+    //         <h2 class="title">${curElm.title}</h2>
+    //         <p class="post-info">${curElm.body}</p>
+    //    </div>
+    //    `;
 
-       container.insertAdjacent(`beforeend`, htmlData)
-   })
+        let div = document.createElement('div');
+        div.className = "posts";
+        
+        let p = document.createElement('p');
+        p.className = "post-id";
 
+        let h2 = document.createElement('h2');
+        h2.className = "title";
 
+        let p2 = document.createElement('p');
+        p2.className = "post-info";
+
+        p.innerHTML = postCount++;
+        h2.innerHTML = curElm.title;
+        p2.innerHTML = curElm.body;
+
+        div.append(p,h2,p2);
+        container.append(div);
+
+   });
 }
 
-
 getPost();
+
+const showData = () =>
+{
+    setTimeout( () =>
+    {
+        pageCount++;
+        getPost();
+    }, 300)
+}
+
+window.addEventListener('scroll',() => 
+{
+    const {scrollHeight, scrollTop, clientHeight } = document.documentElement;
+    
+    if(scrollTop + clientHeight >= scrollHeight)
+    {
+        console.log("Srng khali pohachla");
+        showData();
+    }
+    
+})
